6f06f7ef628056bef1be996fd33549460045e649
JM <jmnavarro@gmail.com>
2012-06-27 17:57:11 +0200

    new test

--- cartoDB_clientTests/CartoDBClientTests.m
+++ cartoDB_clientTests/CartoDBClientTests.m
@@ -43,35 +43,61 @@
 }
 
 
-- (void)testOfflineShouldFailIfSQLIsEmpty
+- (void)testShouldFailIfSQLIsEmpty
 {
     STAssertFalse([_fixture startRequestWithSQL:@""], @"Must fail!");
 }
 
-- (void)testOfflineShouldFailIfSQLIsNil
+- (void)testShouldFailIfSQLIsNil
 {
     STAssertFalse([_fixture startRequestWithSQL:nil], @"Must fail!");
 }
 
-- (void)testOfflineShouldFailIfDelegateIsNotSet
+- (void)testShouldFailIfDelegateIsNotSet
 {
     _fixture.delegate = nil;
     STAssertFalse([_fixture startRequestWithSQL:@"the-sql"], @"Must fail!");
 }
 
-- (void)testOfflineShouldWorkIfClientAndRequestIsOK
+- (void)testShouldWorkIfClientAndRequestIsOKWithFormatJSON
 {
     NSString* json = @"{\"time\":0.011,\"total_rows\":1,\"rows\":[{\"cartodb_id\":1,\"name\":\"STATIC \",\"description\":\"STATIC \",\"created_at\":\"2012-06-21T16:56:24.827Z\",\"updated_at\":\"2012-06-21T16:56:46.903Z\",\"the_geom\":\"0101000020E610000000000000000000000000000000000000\",\"the_geom_webmercator\":\"0101000020110F0000000000000000000000000040A65408BE\"}]}";
     
+    _dataProvider.responseFormat = CartoDBResponseFormat_JSON;
+
     _dataProvider.error = nil;
-    _dataProvider.response = [[CartoDBResponse alloc] initWithJSONResponse:json];
+    _dataProvider.response = [[CartoDBResponse alloc] initWithJSON:json andFormat:_dataProvider.responseFormat];
     
     STAssertTrue([_fixture startRequestWithSQL:@"valid-sql"], @"Cant start request");
     
     // check expectations
     STAssertNil(_lastError, @"Last error must be nil");
     STAssertEquals(_dataProvider.response, _lastResponse, @"Bad response received");
+    STAssertEquals(CartoDBResponseFormat_JSON, _lastResponse.format, @"Bad response format");
 }
+
+
+- (void)testShouldWorkIfClientAndRequestIsOKWithFormatGeoJSON
+{
+    NSString* geojson = @"{\"type\": \"FeatureCollection\",\"features\": [{\"type\": \"Feature\",\"properties\": {\"cartodb_id\": 1, \"name\": \"centro\",\"description\": \"el centro del universo\",\"created_at\": \"2012-06-21T16:56:24.827Z\",\"updated_at\": \"2012-06-21T16:56:46.903Z\"},\"geometry\": {\"type\": \"Point\", \"coordinates\": [10.1,20.2]}}]}";
+    
+    _dataProvider.responseFormat = CartoDBResponseFormat_GeoJSON;
+    
+    _dataProvider.error = nil;
+    _dataProvider.response = [[CartoDBResponse alloc] initWithJSON:geojson andFormat:_dataProvider.responseFormat];
+    
+    STAssertTrue([_fixture startRequestWithSQL:@"valid-sql"], @"Cant start request");
+    
+    // check expectations
+    STAssertNil(_lastError, @"Last error must be nil");
+    STAssertEquals(_dataProvider.response, _lastResponse, @"Bad response received");
+    STAssertEquals(CartoDBResponseFormat_GeoJSON, _lastResponse.format, @"Bad response format");
+}
+
+
+
+
+
 
 
 
