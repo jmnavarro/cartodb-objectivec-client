f7bafb4e9559da14ac204dfbcb4181ccd8ecca81
JM <jmnavarro@gmail.com>
2012-06-27 18:01:22 +0200

    Added to ignore

--- cartoDB_clientTests/YourData.h
+++ cartoDB_clientTests/YourData.h
@@ -10,10 +10,15 @@
 #define cartoDB_client_YourData_h
 
 
-
+/*
 #define kUser   @"YOUR_CARTODB_USER"
 #define kAPIKey @"YOUR_CARTODB_API_KEY"
 #define kTableWithData  @"ONE_OF_YOUR_TABLES_WITH_AT_LEAST_ONE_ROW"
+*/
+
+#define kUser   @"jmnavarro"
+#define kAPIKey @"0202f1cc72750a8c7ff0f2dd31d0f675640830a7"
+#define kTableWithData  @"points"
 
 
 
