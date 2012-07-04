54c8393ea81ad5a70c2378b846db1b1ba6466a58
JM <jmnavarro@gmail.com>
2012-07-05 00:19:02 +0200

    paging in gui

--- cartoDB_client/MainViewController.h
+++ cartoDB_client/MainViewController.h
@@ -9,8 +9,21 @@
 #import <UIKit/UIKit.h>
 
 @interface MainViewController : UIViewController<CartoDBClientDelegate>
+{
+    int _page;
+}
 
 @property (nonatomic, retain) IBOutlet UITextView *text;
 @property (nonatomic, retain) IBOutlet UITextView *result;
+
+@property (nonatomic, retain) IBOutlet UIButton *run;
+@property (nonatomic, retain) IBOutlet UIButton *next;
+@property (nonatomic, retain) IBOutlet UIButton *prev;
+
+@property (nonatomic, retain) IBOutlet UIActivityIndicatorView *spinner;
+
+- (IBAction) runAction;
+- (IBAction) nextAction;
+- (IBAction) prevAction;
 
 @end
--- cartoDB_client/MainViewController.m
+++ cartoDB_client/MainViewController.m
@@ -19,7 +19,7 @@ static NSString* const kTableWithData = @"nyc_wifi";
 
 
 @implementation MainViewController
-@synthesize text, result;
+@synthesize text, result, run, next, prev, spinner;
 
 - (void) viewDidLoad 
 {
@@ -28,22 +28,60 @@ static NSString* const kTableWithData = @"nyc_wifi";
     self.text.text = [NSString stringWithFormat:@"SELECT * FROM %@", kTableWithData]; // <- put your query here 
 }
 
-- (IBAction) click
+- (void) viewDidUnload
+{
+    self.text = nil;
+    self.result = nil;
+    self.run = nil;
+    self.prev = nil;
+    self.next = nil;
+    self.spinner = nil;
+
+    [super viewDidUnload];
+}
+
+- (void) queryForPage:(int)page
 {
     CartoDBCredentials *credentials = kAPIKey ? [[CartoDBCredentialsApiKey alloc] initWithApiKey:kAPIKey] : [[CartoDBCredentials alloc] init];
     credentials.username = kUser;
     
     CartoDBDataProvider *provider = [[CartoDBDataProviderHTTP alloc] init];
     provider.credentials = credentials;
+    provider.page = page;
     [credentials release];
     
     CartoDBClient *client = [[CartoDBClient alloc] init];
     client.provider = provider;
     [provider release];
     client.delegate = self;
-
+    
+    self.run.enabled = NO;
+    self.prev.enabled = NO;
+    self.next.enabled = NO;
+    [self.spinner startAnimating];
+    
     [client startRequestWithSQL:self.text.text];
 }
+
+- (IBAction) runAction
+{
+    _page = 0;
+    [self queryForPage:_page];
+}
+
+- (IBAction) nextAction
+{
+    [self queryForPage:++_page];
+}
+
+- (IBAction) prevAction
+{
+    if (_page > 0) {
+        [self queryForPage:--_page];
+    }
+}
+
+
 
 
 - (void) cartoDBClient:(CartoDBClient*)client receivedResponse:(CartoDBResponse*)response
@@ -52,7 +90,7 @@ static NSString* const kTableWithData = @"nyc_wifi";
     
     NSMutableString *str = [[NSMutableString alloc] initWithCapacity:response.count * 512];
     
-    [str appendFormat:@"CartoDBResponse -> %d rows", response.count];
+    [str appendFormat:@"CartoDBResponse -> %d rows (page %d)", response.count, _page, nil];
     for (int i = 0; i < response.count; ++i) {
         [str appendFormat:@"\n\t %d ->", i];
         
@@ -68,22 +106,25 @@ static NSString* const kTableWithData = @"nyc_wifi";
     
     [str release];
     [client release];
+    
+    [self.spinner stopAnimating];
+    self.run.enabled = YES;
+    self.prev.enabled = YES;
+    self.next.enabled = YES;
 }
 
 
 - (void) cartoDBClient:(CartoDBClient*)client failedWithError:(NSError*)err
 {
-    NSLog(@"Error");
+    self.result.text = [NSString stringWithFormat:@"Error: %@", err];
+
     [client release];    
+    [self.spinner stopAnimating];
+    self.run.enabled = YES;
+    self.prev.enabled = YES;
+    self.next.enabled = YES;
 }
 
-
-- (void) dealloc
-{
-    [self.text release];
-    [self.result release];
-    [super dealloc];
-}
 
 
 @end
